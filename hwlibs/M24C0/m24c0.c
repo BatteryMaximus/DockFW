4bf12b98f8d4b084fb3a6d5554ee7b77bd3b12df
NeatStranger <ashtonm937@gmail.com>
2025-04-10 15:10:44 -0700

    Read and write are working correctly now.

--- hwlibs/M24C0/m24c0.c
+++ hwlibs/M24C0/m24c0.c
@@ -4,34 +4,26 @@
 
 int writeDeviceSerial(uint16_t serialNumber, i2c_inst_t *i2c){
     uint8_t deviceAddress = 0xD0;
-    uint8_t upperByteAddress[] = {0xE1};
-    uint8_t lowerByteAddress[] = {0xF2};
+    uint8_t upperByteAddress[] = {0x01};
+    uint8_t lowerByteAddress[] = {0x02};
     uint8_t upperByte = (serialNumber >> 8) & 0xFF;
     uint8_t lowerByte = serialNumber & 0xFF;
+
     uint8_t devSerialDataFirst[2] = {upperByteAddress[0], upperByte};
     uint8_t devSerialDataLast[2] = {lowerByteAddress[0], lowerByte};
+
+    printf("Lower Byte: %02X \n", lowerByte);
     i2c_write_blocking(i2c, deviceAddress, devSerialDataFirst, 2, false);
+    sleep_ms(10);
     i2c_write_blocking(i2c, deviceAddress, devSerialDataLast, 2, false);
-	
-    
-    //now to confirm the data
-    uint8_t response_data[] = {0x00};
-    i2c_write_blocking(i2c, deviceAddress, upperByteAddress, 1, false);
-    i2c_read_blocking(i2c, deviceAddress, response_data, 1, false);
-    printf("Response 1/2: %02X, ", response_data[0]);
-    response_data[0] = 0x00;
-    i2c_write_blocking(i2c, deviceAddress, lowerByteAddress, 1, false);
-    i2c_read_blocking(i2c, deviceAddress, response_data, 1, false);
-    printf("Response 2/2: %02X \n", response_data[0]);
-    
     return 0;
     
 }
 
 uint16_t readDeviceSerial(i2c_inst_t *i2c){
     uint8_t deviceAddress = 0xD0;
-    uint8_t upperByteAddress[] = {0xE1};
-    uint8_t lowerByteAddress[] = {0xF2};
+    uint8_t upperByteAddress[] = {0x01};
+    uint8_t lowerByteAddress[] = {0x02};
 
     uint8_t response_data[] = {0x00};
     uint16_t serialNumberRead = 0x0000;
